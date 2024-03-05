3ef2c4e52054427daba0f6707078f9f62f1449bf
royalsrivastavagithub <royalsrivastavaofficial@gmail.com>
2024-03-05 15:31:34 +0530

    another small commit i may continue again this night after some important work.

---
+++ javascript/array_destructuring.js
@@ -0,0 +1,13 @@
+const arr = ["v1", "v2", "v3", "v4"];
+//let arr1 = arr[0];
+//let arr2 = arr[1];
+//console.log("value of arr1:", arr1);
+//console.log("value of arr1:", arr2);
+
+//array destructuring
+let [arr1, arr2, arr3] = arr;
+console.log("value of arr1:", arr1);
+console.log("value of arr1:", arr2);
+console.log("value of arr1:", arr3);
+//arr1 get value from index 0 of arr and arr2 will get from index 1 of arr
+//if arry dont have enough values and index for assigning it to variables the variables will get undefined
---
+++ javascript/objects.js
@@ -0,0 +1,16 @@
+//objects is as a dictionary in python.
+//its a reference data type just like array
+
+const person = {
+  name: "royal",
+  age: 21,
+  hobbies: ["football", "music"],
+};
+
+console.log("name:", person.name);
+console.log("age:", person.age);
+console.log("hobbies:", person.hobbies);
+//objects can store array
+
+person.gender = "male";
+console.log("gender:", person.gender);

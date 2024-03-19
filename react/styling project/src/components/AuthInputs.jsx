d9883f5abc132f6b5d0763cfcb5c97be3752fa01
royalsrivastavagithub <royalsrivastavaofficial@gmail.com>
2024-03-19 18:35:04 +0530

    small commit

--- react/styling project/src/components/AuthInputs.jsx
+++ react/styling project/src/components/AuthInputs.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import {styled} from 'styled-components'
+
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
--- react/styling project/src/components/Header.jsx
+++ react/styling project/src/components/Header.jsx
@@ -1,10 +1,16 @@
-import logo from '../assets/logo.png';
-import './Header.css'
+import logo from "../assets/logo.png";
+import "./Header.css";
 export default function Header() {
   return (
-    <header>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
+    <header className="flex flex-col items-center mt-8 mb-16ss">
+      <img
+        src={logo}
+        alt="A canvas"
+        className="mb-8 w-44 h-44 object-contain"
+      />
+      <h1 className="text-4xl font-semibold tracking-wiest text-center uppercase text-amber-800">
+        ReactArt
+      </h1>
       <p>A community of artists and art-lovers.</p>
     </header>
   );

18679cd63b8525b45b8291a35ca34a9c1955b24f
royalsrivastavagithub <royalsrivastavaofficial@gmail.com>
2025-07-31 06:12:56 +0530

    pointer.cpp

--- DSA/pointers/pointers.cpp
+++ DSA/pointers/pointers.cpp
@@ -5,16 +5,17 @@ using namespace std;
 int main()
 {
     int num = 10;
-    int *pointer;
-    pointer = &num;//init
+    int *pointer;   // declaration of pointer
+    pointer = &num; // initization of pointer
     cout << "num = " << num << endl;
     cout << "pointer = " << pointer << endl;
-    cout << "pointer value = " << *pointer << endl;//dereferencing with *
+    cout << "pointer value = " << *pointer << endl; // dereferencing of pointer
     num = num - 1;
     cout << "pointer value after [num-1] = " << *pointer << endl;
     *pointer = *pointer + 1;
     cout << "pointer value = after [*pointer+1] = " << *pointer << endl;
-    pointer = pointer-1;
+    pointer = pointer - 1;
     cout << "pointer after [pointer-1] = " << pointer << endl;
     cout << "pointer value after [pointer-1] = " << *pointer << endl;
+    return 0;
 }

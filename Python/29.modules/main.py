121e9ec75872b4b6cc29e52438b4ae98b80cfaff
royalsrivastavagithub <royalsrivastavaofficial@gmail.com>
2025-09-02 20:19:42 +0530

    a little bit of more python

---
+++ Python/29.modules/main.py
@@ -0,0 +1,7 @@
+import math as m
+
+print(m.pi)
+print(m.e)
+print(m.sqrt(2))
+print(m.ceil(0.1))
+print(m.floor(0.9))
---
+++ Python/30.scope_resolution/main.py
@@ -0,0 +1,11 @@
+def func1():
+    print(x)
+
+def func2():
+    print(x)
+
+
+x = 3 
+
+func1()
+func2()
